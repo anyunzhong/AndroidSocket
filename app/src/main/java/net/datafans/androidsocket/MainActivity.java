809b3b14e6230acbb27ff56c00b9ca578bdf48de
Fast-Dev-Kit <2642754767@qq.com>
2015-06-06 11:53:48 +0800

    build

--- app/src/main/java/net/datafans/androidsocket/MainActivity.java
+++ app/src/main/java/net/datafans/androidsocket/MainActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import io.netty.bootstrap.Bootstrap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     @Override
